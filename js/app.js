e0dcc31f611695d759bda8c68e3aa8863c93ed2b
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-10 16:29:35 +0100

    add bonus 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 const app = Vue.createApp({
   data() {
     return {
@@ -45,6 +50,10 @@ const app = Vue.createApp({
       } else {
         this.activeImage++;
       }
+    },
+    // Bonus 1
+    changeImage(index) {
+      this.activeImage = index;
     },
   },
 });
